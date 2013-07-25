dccc88085370588b8af9c0fb41a08c89cd76646c
Michael Pope <michael@dtcorp.com.au>
2013-07-25 16:34:25 +1000

    Add route for api tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 AngularRailsExample::Application.routes.draw do
   get "home/index"
 
+  namespace :api do
+    resources :tasks
+  end
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
